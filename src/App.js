c414fe175753f799ea03e4443e069c5f36469540
houssamelmalki-work <houssamelmalkiwork@gmail.com>
2026-06-06 11:26:19 +0100

    update : video

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
 import Home from "./routes/Home";
 import ArticlePage from "./routes/ArticlePage";
-import ContactPage from "./routes/ContactPage";
 import CategoryPage from "./routes/CategoryPage";
+import ContactPage from "./routes/ContactPage";
 
 function App() {
+  const location = useLocation();
+
+  // ✅ Envoie chaque visite de page à Google Analytics
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page:    location.pathname,
+    });
+  }, [location]);
+
   return (
     <Routes>
-      <Route path="/"                  element={<Home />} />
-      <Route path="/article/:id"       element={<ArticlePage />} />
-      <Route path="/category/:cat"     element={<CategoryPage />} />
-      <Route path="/contact"           element={<ContactPage />} />
+      <Route path="/"              element={<Home />} />
+      <Route path="/article/:id"   element={<ArticlePage />} />
+      <Route path="/category/:cat" element={<CategoryPage />} />
+      <Route path="/contact"       element={<ContactPage />} />
     </Routes>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { LanguageProvider } from "./context/LanguageContext";
+import ReactGA from "react-ga4";
+
+// ✅ Initialise Google Analytics
+ReactGA.initialize("G-JQ51LD3147");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
